2164775c6f1269d0200c604c92c254ec813013bc
Paul <lambdapaul@protonmail.com>
2022-02-15 22:24:17 -0500

    Change 404 tag

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,13 +4,13 @@ function NotFoundPage() {
 
     return (
         <Layout name="... ??? / 404: Not Found">
-            <div className="block">
+            <section className="block">
                 <h1>Error 404: Not Found</h1>
                 <p>
                     <strong>Uh oh! The page you are looking for does not exist...</strong><br />
                     <strong><a href="https://en.wikipedia.org/wiki/List_of_HTTP_status_codes">[Wikipedia] Learn more about HTTP status codes.</a></strong>
                 </p>
-            </div>
+            </section>
         </Layout>
     );
 }
